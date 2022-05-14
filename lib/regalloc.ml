c324e9623e87ae3c3f21943984268791edfd56cb
tianboh <haotianbo.htb@alibaba-inc.com>
2022-05-14 02:45:18 +0000

    Done:   1. Implemented mutually linked adjacency list.   2. Implemented sorted keys for print adj lists. TODO:   1. Implement SEO

--- lib/regalloc.ml
+++ lib/regalloc.ml
@@ -16,12 +16,11 @@ let (<?>) c (cmp, x, y) =
   else c;;
 
 let reg_cmp (t1 : string) (t2 : string) = 
-  let s1 = String.sub t1 ~pos:2 ~len:((String.length t1) -2) in
-  let s2 = String.sub t2 ~pos:2 ~len:((String.length t2) -2) in
-  String.compare (String.sub t1 ~pos:1 ~len:1) (String.sub t2 ~pos:1 ~len:1)
-  <?> (Int.compare, int_of_string s1, int_of_string s2)
+  Int.compare (String.length t1) (String.length t2)
+  <?> (String.compare, t1, t2)
 
-let rec build_adj_hash (prog : program) hash = match prog with 
+(* Build single linked adjacency list based on original .in file. *)
+let rec build_ori_adj_hash (prog : program) hash = match prog with 
   | [] -> hash
   | h :: t -> 
     let s1 = match Hashtbl.find hash h.define with 
@@ -32,18 +31,51 @@ let rec build_adj_hash (prog : program) hash = match prog with
     match h.define with
     |"" -> hash
     |_ -> Hashtbl.set hash ~key: h.define ~data: s_u;
-      build_adj_hash t hash;
+      build_ori_adj_hash t hash;
 ;;
 
-let print_adj adj = Hashtbl.iteri adj ~f:(fun ~key ~data -> 
-    let l = List.sort (Set.to_list data) ~compare:reg_cmp in
-    let () = printf "\nfrom %s to\t" key in 
-    List.iter l ~f:(fun x -> printf "%s " x));
+(* Build double(mutually) linked adjacency list based on .in file.*)
+let rec build_mut_adj_hash (prog : program) hash = match prog with 
+  | [] -> hash
+  | h :: t -> 
+    let s1 = match Hashtbl.find hash h.define with 
+      | Some s -> s
+      | None -> Set.empty (module String) in
+    let s2 = Set.of_list (module String) h.live_out in
+    let s_u = Set.union s1 s2 in
+    match h.define with
+    |"" -> hash
+    |_ -> 
+      Hashtbl.set hash ~key: h.define ~data: s_u;
+      Set.iter s2 ~f:(fun key -> 
+          let s_h = Set.of_list (module String) [h.define] in
+          let s_old =  
+            match Hashtbl.find hash key with
+            |None -> Set.empty (module String)
+            |Some s-> s in
+          let s_new = Set.union s_old s_h in
+          Hashtbl.set hash ~key: key ~data: s_new);
+      build_mut_adj_hash t hash;
+;;
+
+let print_adj adj = 
+  let keys = Hashtbl.keys adj in
+  let sorted_keys = List.sort keys ~compare:reg_cmp in
+  let () = List.iter sorted_keys 
+      ~f:(fun key -> 
+          let s = Hashtbl.find adj key in
+          let s_ = match s with
+            |None -> failwith "empty adjacency list"
+            |Some s -> s in
+          let l = List.sort (Set.to_list s_) ~compare:reg_cmp in
+          printf "\nFrom %s to\t" key;
+          List.iter l ~f:(fun x -> printf "%s " x)
+        ) in
   printf "\n\n";
 ;;
 
 let regalloc (_program : program) : allocations =
   let hash = Hashtbl.create (module String) in
-  let adj = build_adj_hash _program hash in
+  let adj = build_mut_adj_hash _program hash in
   let () = print_adj adj in
   failwith "Implement regalloc here";;
--- lib/regalloc.mli
+++ lib/regalloc.mli
@@ -7,4 +7,12 @@ type node =
 
 type interference_graph = (node * node list) list;;
 
+val build_ori_adj_hash : program ->
+  (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t ->
+  (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t
+
+val build_mut_adj_hash : program ->
+  (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t ->
+  (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t
+
 val regalloc : program -> allocations
