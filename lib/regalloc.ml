d13a7516ab80ba0e42b1485e07e65bf8c437c4bb
tianboh <haotianbo.htb@alibaba-inc.com>
2022-05-16 22:20:54 +0000

    Done   1. Implemented Simplicial Elimination Order(SEO) with complexity O(n^2) where n is the number of nodes in interferance graph. TODO   1. Implement greedy coloring.   2. Implement scheme for color to standard output format.

--- lib/regalloc.ml
+++ lib/regalloc.ml
@@ -2,14 +2,6 @@ open Core
 open Lab1_checkpoint
 open Printf
 
-type node = 
-  { reg : reg
-  ; mutable order : int (* order for SEO *)
-  ; mutable is_alloc : bool (* whether is allocated during SEO*)
-  }
-
-type interference_graph = (node * node list) list;;
-
 let (<?>) c (cmp, x, y) = 
   if c = 0
   then cmp x y
@@ -20,42 +12,75 @@ let reg_cmp (t1 : string) (t2 : string) =
   <?> (String.compare, t1, t2)
 
 (* Build single linked adjacency list based on original .in file. *)
-let rec build_ori_adj_hash (prog : program) hash = match prog with 
-  | [] -> hash
-  | h :: t -> 
-    let s1 = match Hashtbl.find hash h.define with 
-      | Some s -> s
-      | None -> Set.empty (module String) in
-    let s2 = Set.of_list (module String) h.live_out in
-    let s_u = Set.union s1 s2 in
-    match h.define with
-    |"" -> hash
-    |_ -> Hashtbl.set hash ~key: h.define ~data: s_u;
-      build_ori_adj_hash t hash;
+let build_ori_adj_hash (prog : program) =
+  let hash = Hashtbl.create (module String) in 
+  let rec helper prog hash = match prog with 
+    | [] -> hash
+    | h :: t -> 
+      let s1 = match Hashtbl.find hash h.define with 
+        | Some s -> s
+        | None -> Set.empty (module String) in
+      let s2 = Set.of_list (module String) h.live_out in
+      let s_u = Set.union s1 s2 in
+      match h.define with
+      | "" -> hash
+      | _ -> 
+        Hashtbl.set hash ~key: h.define ~data: s_u;
+        helper t hash;
+  in helper prog hash
 ;;
 
 (* Build double(mutually) linked adjacency list based on .in file.*)
-let rec build_mut_adj_hash (prog : program) hash = match prog with 
-  | [] -> hash
-  | h :: t -> 
-    let s1 = match Hashtbl.find hash h.define with 
-      | Some s -> s
-      | None -> Set.empty (module String) in
-    let s2 = Set.of_list (module String) h.live_out in
-    let s_u = Set.union s1 s2 in
-    match h.define with
-    |"" -> hash
-    |_ -> 
-      Hashtbl.set hash ~key: h.define ~data: s_u;
-      Set.iter s2 ~f:(fun key -> 
-          let s_h = Set.of_list (module String) [h.define] in
-          let s_old =  
-            match Hashtbl.find hash key with
-            |None -> Set.empty (module String)
-            |Some s-> s in
-          let s_new = Set.union s_old s_h in
-          Hashtbl.set hash ~key: key ~data: s_new);
-      build_mut_adj_hash t hash;
+let build_mut_adj_hash (prog : program) = 
+  let hash = Hashtbl.create (module String) in
+  let rec helper prog hash = match prog with 
+    | [] -> hash
+    | h :: t -> 
+      let s1 = match Hashtbl.find hash h.define with 
+        | Some s -> s
+        | None -> Set.empty (module String) in
+      let s2 = Set.of_list (module String) h.live_out in
+      let s_u = Set.union s1 s2 in
+      match h.define with
+      | "" -> hash
+      | _ -> 
+        Hashtbl.set hash ~key: h.define ~data: s_u;
+        Set.iter s2 ~f:(fun key -> 
+            match key with
+            | "" -> () (* Avoid empty key match to a list of nodes. *)
+            | _ -> 
+              let s_h = Set.of_list (module String) [h.define] in
+              let s_old =  
+                match Hashtbl.find hash key with
+                |None -> Set.empty (module String)
+                |Some s-> s in
+              let s_new = Set.union s_old s_h in
+              Hashtbl.set hash ~key: key ~data: s_new);
+        helper t hash;
+  in helper prog hash
+;;
+
+let gen_line_to_tmp prog = 
+  let hash = Hashtbl.create (module Int) in
+  let rec helper prog hash = match prog with
+    | [] -> hash
+    | h :: t -> 
+      Hashtbl.set hash ~key:h.line_number ~data:h.define;
+      helper t hash;
+  in helper prog hash
+;;
+
+let gen_reg_table (prog : program) = 
+  let hash = Hashtbl.create (module String) in
+  let rec helper prog hash = match prog with
+    | [] -> hash
+    | h::t -> 
+      match h.define with
+      |"" -> helper t hash;
+      | _ ->
+        Hashtbl.set hash ~key:h.define ~data:0;
+        helper t hash;
+  in helper prog hash
 ;;
 
 let print_adj adj = 
@@ -63,19 +88,65 @@ let print_adj adj =
   let sorted_keys = List.sort keys ~compare:reg_cmp in
   let () = List.iter sorted_keys 
       ~f:(fun key -> 
-          let s = Hashtbl.find adj key in
-          let s_ = match s with
-            |None -> failwith "empty adjacency list"
-            |Some s -> s in
-          let l = List.sort (Set.to_list s_) ~compare:reg_cmp in
+          let s = Hashtbl.find_exn adj key in
+          let l = List.sort (Set.to_list s) ~compare:reg_cmp in
           printf "\nFrom %s to\t" key;
           List.iter l ~f:(fun x -> printf "%s " x)
         ) in
   printf "\n\n";
 ;;
 
-let regalloc (_program : program) : allocations =
-  let hash = Hashtbl.create (module String) in
-  let adj = build_mut_adj_hash _program hash in
+let seo adj reg_table = 
+  let seq = [] in
+  let rec helper adj reg_table seq = 
+    let () = printf "\nhelper seq\n" in
+    let () = List.iter ~f:(printf "%s ") seq in 
+    let () = printf "\ncurrent graph order\n" in
+    let () = Hashtbl.iteri reg_table ~f:(fun ~key ~data -> printf "%s %d\n" key data) in
+    match Hashtbl.is_empty reg_table with
+    | true -> seq
+    | false -> 
+      let (u, ord_) = match Hashtbl.fold reg_table ~init:None 
+                              ~f:(fun ~key ~data accu -> 
+                                  let () = printf "  iterate reg_table %s %d\n" key data in
+                                  match accu with
+                                  | None -> 
+                                    let () = printf "  accu initialize with %s %d\n" key data in
+                                    Some (key, data)
+                                  | Some (key', data') -> 
+                                    let () = printf "  current accu %s %d\n" key' data' in
+                                    if data' < data 
+                                    then 
+                                      let () = printf "  accu not change\n" in
+                                      accu
+                                    else 
+                                      let () = printf "  find smaller value with %s %d\n" key data in
+                                      Some (key, data)
+                                ) with
+      | None -> failwith "empty reg_table"
+      | Some s -> s in 
+      let seq_new = seq@[u] in
+      let nbr = Hashtbl.find_exn adj u in
+      let nbr = Set.remove nbr u in
+      printf "find node %s with order %d\n" u ord_;
+      Set.iter nbr ~f:(fun x -> 
+          match Hashtbl.find reg_table x with
+          | None -> ()
+          | Some v -> 
+            let order = v + 1 in
+            Hashtbl.set reg_table ~key:x ~data:order;
+            printf "---> %s update node %s's order to %d\n" u x order);
+      Hashtbl.remove reg_table u;
+      helper adj reg_table seq_new;
+  in helper adj reg_table seq
+;;
+
+let regalloc (prog : program) : allocations =
+  let adj = build_mut_adj_hash prog in
   let () = print_adj adj in
+  let reg_table = gen_reg_table prog in
+  let seq = seo adj reg_table in
+  let () = List.iter ~f:(printf "%s ") seq in
+  let () = printf "\n" in
+  (* let line_to_tmp = gen_line_to_tmp prog in *)
   failwith "Implement regalloc here";;
--- lib/regalloc.mli
+++ lib/regalloc.mli
@@ -1,18 +1,15 @@
 open Lab1_checkpoint
-type node = 
-  { reg : reg
-  ; mutable order : int (* order for SEO *)
-  ; mutable is_alloc : bool (* whether is allocated during SEO*)
-  }
-
-type interference_graph = (node * node list) list;;
 
 val build_ori_adj_hash : program ->
-  (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t ->
   (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t
 
 val build_mut_adj_hash : program ->
-  (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t ->
   (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t
+
+val gen_line_to_tmp : line list -> (int, reg) Base.Hashtbl.t
+
+val gen_reg_table : program -> (reg, int) Base.Hashtbl.t
+
+val seo : (reg, (reg, 'a) Base.Set.t) Base.Hashtbl.t -> (reg, int) Base.Hashtbl.t -> reg list
 
 val regalloc : program -> allocations
