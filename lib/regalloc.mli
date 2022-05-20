060856ccb486acc3722b76a2f90c08b2765b931c
tianboh <haotianbo.htb@alibaba-inc.com>
2022-05-20 00:53:46 +0000

    Fixed   1. Fixed a bug for maximum cardinality search. Used to select minimum weight in each iteration   2. Isolated %eax and %edx in interferance graph. Edges will only connect temporaries. This can make interferance graph a pure SSA. As for lines whose define field is %eax or %edx, we will not explicit allocate any registers for them, which means they will use %eax and %edx implicitly. This can reduce the number of use of explicit allocated registers. Added   1. Header file for regalloc.ml.

--- lib/regalloc.mli
+++ lib/regalloc.mli
@@ -3,15 +3,11 @@ open Lab1_checkpoint
 val build_graph : program ->
   (reg, (reg, Base.String.comparator_witness) Base.Set.t) Base.Hashtbl.t
 
-val gen_line_to_tmp : line list -> (int, reg) Base.Hashtbl.t
-
-val gen_reg_table : program -> (reg, int) Base.Hashtbl.t
+val gen_reg_table : line list -> (reg, int) Base.Hashtbl.t
 
 val print_adj : (reg, (reg, 'a) Base.Set.t) Base.Hashtbl.t -> unit
 
-val pre_color : line list -> (reg, reg) Base.Hashtbl.t
-
-val seo : (reg, (reg, 'a) Base.Set.t) Base.Hashtbl.t -> program -> reg list
+val seo : (reg, (reg, 'a) Base.Set.t) Base.Hashtbl.t -> line list -> reg list
 
 val greedy : reg list ->
   (reg, (reg, 'a) Base.Set.t) Base.Hashtbl.t ->
