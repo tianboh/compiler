0d420948ab69f57acdd768c9c2c871ae08b9f5ff
Tianbo Hao <tianboh@alumni.cmu.edu>
2023-05-22 05:50:05 +0000

    [Feature] L3 controlflow check
    Check controlflow in each possible branch
    
    1) Whether variable is used before definition
    
    2) Whether each possible branch has return. If the function returns
    void, then omission for return is allowed.
    
    [Feature] L3 Typecheck

--- lab3/semantic/controlflow.ml
+++ lab3/semantic/controlflow.ml
@@ -3,7 +3,8 @@
  *
  * Control flow check is part of semantic analysis.
  * It will check
- * 1) Whether each AST element has return
+ * 1) Whether each AST element has return, except function
+ * with void return type.
  * 2) Whether a variable is used before initialization.
  * 
  * Author: Tianbo Hao <tianboh@alumni.cmu.edu>
@@ -13,7 +14,19 @@ open Core
 module A = Front.Ast
 module S = Util.Symbol.Map
 module Symbol = Util.Symbol
+module Set = Util.Symbol.Set
 module Mark = Util.Mark
+module TC = Typechecker
+
+type param = A.param
+
+(* env records defined(initialized) variables and declared variables in a function.
+ * defined variables are subset of declared variables.
+ * ret_sig is the expected return type of this function. *)
+type env =
+  { var_def : Set.t
+  ; var_decl : Set.t
+  }
 
 let tc_errors : Util.Error_msg.t = Util.Error_msg.create ()
 
@@ -26,95 +39,108 @@ let error ~msg src_span =
  * This is to make sure each path from the beginning of the
  * program will terminate with an return. 
  *
+ * Don't worry about the return type and required return type
+ * mismatch, this is checked in typechecker. Here, we only
+ * make sure that each branch has returned if required return
+ * type is not void.
+ *
  * Notice that While is treated as never return because we cannot
  * check every case, like how many times it will run in real time.
  * So we treat it conservatively as never return *)
-let rec _cf_ret (ast : A.program) : bool =
+let rec _cf_ret_stm (ast : A.mstm) : bool =
   match Mark.data ast with
-  | A.Assign _ -> false
-  | A.If if_ast -> _cf_ret if_ast.true_stm && _cf_ret if_ast.false_stm
-  | A.While _ -> false
-  | A.Return _ -> true
-  | A.Nop -> false
-  | A.Seq seq_ast -> _cf_ret seq_ast.head || _cf_ret seq_ast.tail
-  | A.Declare decl_ast -> _cf_ret decl_ast.tail
-  | Sexp _ -> false
+  | Assign _ -> false
+  | If if_ast -> _cf_ret_stm if_ast.true_stm && _cf_ret_stm if_ast.false_stm
+  | While _ -> false
+  | Return _ -> true
+  | Nop -> false
+  | Seq seq_ast -> _cf_ret_stm seq_ast.head || _cf_ret_stm seq_ast.tail
+  | Declare decl_ast -> _cf_ret_stm decl_ast.tail
+  | Sexp _ | Assert _ -> false
 ;;
 
-let cf_ret (ast : A.program) : unit =
-  if _cf_ret ast then () else error ~msg:(sprintf "Some branch does not return") None
+let rec cf_ret (ast : A.program) : unit =
+  match ast with
+  | [] -> ()
+  | h :: t ->
+    (match h with
+    | Fdefn fdefn ->
+      (match fdefn.ret_type with
+      | Void -> ()
+      | _ ->
+        if _cf_ret_stm fdefn.blk then () else error ~msg:"Some branches not return" None)
+    | Typedef _ | Fdecl _ ->
+      ();
+      cf_ret t)
 ;;
-
-(* env records defined(initialized) variables and declared variables.
- * defined variables are subset of declared variables. *)
-type env =
-  { var_def : Symbol.Set.t
-  ; var_decl : Symbol.Set.t
-  }
 
 (* Check whether var is used in exp. Return true if used, false otherwise. *)
 let rec use (exp : A.mexp) (var : Symbol.t) : bool =
   match Mark.data exp with
-  | A.Var var' -> phys_equal var' var
-  | A.Const_int _ -> false
-  | A.True -> false
-  | A.False -> false
-  | A.Binop binop -> use binop.lhs var || use binop.rhs var
-  | A.Terop terop ->
-    use terop.cond var || use terop.true_exp var || use terop.false_exp var
+  | Var var' -> phys_equal var' var
+  | Const_int _ -> false
+  | True -> false
+  | False -> false
+  | Binop binop -> use binop.lhs var || use binop.rhs var
+  | Terop terop -> use terop.cond var || use terop.true_exp var || use terop.false_exp var
+  | Fcall fcall -> List.fold fcall.args ~init:false ~f:(fun acc arg -> acc || use arg var)
 ;;
 
 (* Check whether variable var is live in statement stm
  * Notice that liveness analysis does not require env.
  * We only need to check whether var is used on RHS of the expression *)
-let rec live (stm : A.program) (var : Symbol.t) : bool =
+let rec live (stm : A.mstm) (var : Symbol.t) : bool =
   match Mark.data stm with
-  | A.Assign asn_ast -> use asn_ast.value var
-  | A.If if_ast ->
+  | Assign asn_ast -> use asn_ast.value var
+  | If if_ast ->
     use if_ast.cond var || live if_ast.true_stm var || live if_ast.false_stm var
-  | A.While while_ast -> use while_ast.cond var || live while_ast.body var
-  | A.Return ret_ast -> use ret_ast var
-  | A.Nop -> false
-  | A.Seq seq_ast ->
+  | While while_ast -> use while_ast.cond var || live while_ast.body var
+  | Return ret_ast ->
+    (match ret_ast with
+    | None -> false
+    | Some s -> use s var)
+  | Nop -> false
+  | Seq seq_ast ->
     live seq_ast.head var || (live seq_ast.tail var && not (define seq_ast.head var))
-  | A.Declare decl_ast ->
+  | Declare decl_ast ->
     if phys_equal var decl_ast.name then false else live decl_ast.tail var
   | Sexp sexp_ast -> use sexp_ast var
+  | Assert ast -> use ast var
 
 (* Check whether stm defines var *)
-and define (stm : A.program) (var : Symbol.t) : bool =
+and define (stm : A.mstm) (var : Symbol.t) : bool =
   match Mark.data stm with
-  | A.Assign asn_ast -> if phys_equal asn_ast.name var then true else false
-  | A.If if_ast -> define if_ast.true_stm var && define if_ast.false_stm var
-  | A.While _ -> false
-  | A.Return _ -> true
-  | A.Nop -> false
-  | A.Seq seq_ast -> define seq_ast.head var || define seq_ast.tail var
-  | A.Declare decl_ast ->
+  | Assign asn_ast -> if phys_equal asn_ast.name var then true else false
+  | If if_ast -> define if_ast.true_stm var && define if_ast.false_stm var
+  | While _ -> false
+  | Return _ -> true
+  | Seq seq_ast -> define seq_ast.head var || define seq_ast.tail var
+  | Declare decl_ast ->
     if phys_equal decl_ast.name var then false else define decl_ast.tail var
-  | Sexp _ -> false
+  | Sexp _ | Assert _ | Nop -> false
 ;;
 
 (* This is an expression level check. It is called by
- * _cf_init and check whether variable in exp is used
+ * cf_stm and check whether variable in exp is used
  * before any declaration(initialization) *)
-let rec cf_init_exp (exp : A.mexp) (env : env) : unit =
+let rec cf_exp (exp : A.mexp) (env : env) : unit =
   let loc = Mark.src_span exp in
   match Mark.data exp with
-  | A.Var var ->
-    if Symbol.Set.mem env.var_def var
+  | Var var ->
+    if Set.mem env.var_def var
     then ()
     else error ~msg:(sprintf "var %s is used before decl" (Symbol.name var)) loc
-  | A.Const_int _ -> ()
-  | A.True -> ()
-  | A.False -> ()
-  | A.Binop binop ->
-    let () = cf_init_exp binop.lhs env in
-    cf_init_exp binop.rhs env
-  | A.Terop terop ->
-    let () = cf_init_exp terop.cond env in
-    let () = cf_init_exp terop.true_exp env in
-    cf_init_exp terop.false_exp env
+  | Const_int _ -> ()
+  | True -> ()
+  | False -> ()
+  | Binop binop ->
+    cf_exp binop.lhs env;
+    cf_exp binop.rhs env
+  | Terop terop ->
+    cf_exp terop.cond env;
+    cf_exp terop.true_exp env;
+    cf_exp terop.false_exp env
+  | Fcall fcall -> List.iter fcall.args ~f:(fun arg -> cf_exp arg env)
 ;;
 
 (* On each control flow path through the program connecting 
@@ -133,47 +159,68 @@ let rec cf_init_exp (exp : A.mexp) (env : env) : unit =
  *
  * return: env, the updated environmant after executing current AST stm.
  *)
-let rec _cf_init (ast : A.program) (env : env) : env =
+let rec cf_stm (ast : A.mstm) (env : env) : env =
   let loc = Mark.src_span ast in
   match Mark.data ast with
-  | A.Assign asn_ast ->
-    let () = cf_init_exp asn_ast.value env in
-    { env with var_def = Symbol.Set.add env.var_def asn_ast.name }
-  | A.If if_ast ->
-    let () = cf_init_exp if_ast.cond env in
-    let env1 = _cf_init if_ast.true_stm env in
-    let env2 = _cf_init if_ast.false_stm env in
-    { env with var_def = Symbol.Set.inter env1.var_def env2.var_def }
-  | A.While while_ast ->
-    let _ = (_cf_init while_ast.body env : env) in
+  | Assign asn_ast ->
+    cf_exp asn_ast.value env;
+    { env with var_def = Set.add env.var_def asn_ast.name }
+  | If if_ast ->
+    cf_exp if_ast.cond env;
+    let env1 = cf_stm if_ast.true_stm env in
+    let env2 = cf_stm if_ast.false_stm env in
+    { env with var_def = Set.inter env1.var_def env2.var_def }
+  | While while_ast ->
+    ignore (cf_stm while_ast.body env : env);
     env
-  | A.Return _ -> { env with var_def = env.var_decl }
-  | A.Nop -> env
-  | A.Seq seq_ast ->
-    let env' = _cf_init seq_ast.head env in
-    _cf_init seq_ast.tail env'
-  | A.Declare decl_ast ->
+  | Return _ -> { env with var_def = env.var_decl }
+  | Nop -> env
+  | Seq seq_ast ->
+    let env' = cf_stm seq_ast.head env in
+    cf_stm seq_ast.tail env'
+  | Declare decl_ast ->
     if live decl_ast.tail decl_ast.name
     then
       error ~msg:(sprintf "var %s is live in decl scope" (Symbol.name decl_ast.name)) loc
     else (
-      let env' = { env with var_decl = Symbol.Set.add env.var_decl decl_ast.name } in
-      let env_inside = _cf_init decl_ast.tail env' in
+      let env' = { env with var_decl = Set.add env.var_decl decl_ast.name } in
+      let env_inside = cf_stm decl_ast.tail env' in
       (* We keep track of the declare variable as the same as before because
        * declaration inside inner scope shouldn't reflect to outside scope.
        * As for def, we intersect env.var_def and env_inside.var_def because 
        * env_inside.var_def may contain some variable declared in inner scope *)
       { var_decl = env.var_decl
-      ; var_def =
-          Symbol.Set.diff env_inside.var_def (Symbol.Set.of_list [ decl_ast.name ])
+      ; var_def = Set.diff env_inside.var_def (Set.of_list [ decl_ast.name ])
       })
   | Sexp sexp ->
-    let () = cf_init_exp sexp env in
+    cf_exp sexp env;
+    env
+  | Assert aexp ->
+    cf_exp aexp env;
     env
 ;;
 
-let cf_init (ast : A.program) : unit =
-  let env = { var_decl = Symbol.Set.empty; var_def = Symbol.Set.empty } in
-  let _ = (_cf_init ast env : env) in
-  ()
+(* Don't worry about parameter name conflict, this is checked in typechecker. *)
+let init_func_pars (pars : param list) (env : env) : env =
+  List.fold_left pars ~init:env ~f:(fun env_acc par ->
+      let var = par.i in
+      { var_def = Set.add env_acc.var_def var; var_decl = Set.add env_acc.var_decl var })
+;;
+
+let cf_fdefn pars blk env =
+  let env = init_func_pars pars env in
+  ignore (cf_stm blk env : env)
+;;
+
+let rec cf_init (ast : A.program) =
+  match ast with
+  | [] -> ()
+  | h :: t ->
+    (match h with
+    | Fdefn fdefn ->
+      let env = { var_decl = Set.empty; var_def = Set.empty } in
+      cf_fdefn fdefn.pars fdefn.blk env
+    | Fdecl _ | Typedef _ ->
+      ();
+      cf_init t)
 ;;
--- lab3/semantic/typechecker.ml
+++ lab3/semantic/typechecker.ml
@@ -1,8 +1,7 @@
-(* L2 Compiler
+(* L3 Compiler
  * TypeChecker
  *
- * Author: Tianbo Hao <tianboh@alumni.cmu.edu>
- * Created: Alex Vaynberg <alv@andrew.cmu.edu>
+ * Author: Alex Vaynberg <alv@andrew.cmu.edu>
  * Modified: Frank Pfenning <fp@cs.cmu.edu>
  * Converted to OCaml by Michael Duggan <md5i@cs.cmu.edu>
  *
@@ -22,12 +21,20 @@
  * Expression level check
  * 1) Check whether the operand and operator consistent with each other.
  * 
+ * Function level check
+ * 1) Whether each function is defined before called
+ * 2) Whether the calling exp argument type and defined signature match
+ * 3) Whether func parameter conflict with local variables
+ * 4) Whether some functions are defined several time
+ *
  * We can summarize above checks to two cases
  * 1) Type check in statement and expression
  * 2) Variable declaration check, including re-decl and non-decl.
  *
+ * Type alias is already handled in elab, so we don't bother it here.
+ *
  * Modified: Anand Subramanian <asubrama@andrew.cmu.edu> Fall 2010
- * Now distinguishes between declarations and initialization
+ * Now distinguishes between declarations and definition(initialization)
  * Modified: Maxime Serrano <mserrano@andrew.cmu.edu> Fall 2014
  * Should be more up-to-date with modern spec.
  * Modified: Matt Bryant <mbryant@andrew.cmu.edu> Fall 2015
@@ -35,30 +42,37 @@
  * Modified: Alice Rao <alrao@andrew.cmu.edu> Fall 2017
  * Modified: Nick Roberts <nroberts@alumni.cmu.edu> Fall 2018
  * Use records, redo marks.
+ * Modified: Tianbo Hao <tianboh@alumni.cmu.edu> May 2023
  *)
 
 open Core
 module A = Front.Ast
 module S = Util.Symbol.Map
+module Set = Util.Symbol.Set
 module Symbol = Util.Symbol
 module Mark = Util.Mark
 
 type state =
   | Decl
-  | Init
+  | Defn
 
-type dtype =
-  | Int
-  | Bool
+type dtype = A.dtype
+type param = A.param
 
 type var =
   { state : state
   ; dtype : dtype
   }
 
+type func =
+  { state : state
+  ; pars : param list
+  ; ret_type : dtype
+  }
+
 type env =
-  { vars : var S.t
-  ; returns : bool
+  { vars : var S.t (* variable decl/def tracker *)
+  ; funcs : func S.t (* function signature *)
   }
 
 let tc_errors : Util.Error_msg.t = Util.Error_msg.create ()
@@ -68,15 +82,31 @@ let error ~msg src_span =
   raise Util.Error_msg.Error
 ;;
 
-let type_cmp t1 t2 =
+let type_cmp (t1 : dtype) (t2 : dtype) =
   match t1, t2 with
   | Int, Int -> true
-  | Int, Bool -> false
-  | Bool, Int -> false
   | Bool, Bool -> true
+  | Void, Void -> true
+  | _ -> false
 ;;
 
-(* tc_exp will check if expression is valid
+(* Check whether element in params1 and params2 has same dtype respectively *)
+let tc_signature (params1 : dtype list) (params2 : dtype list) func_name =
+  let func_name_s = Symbol.name func_name in
+  let param =
+    match List.zip params1 params2 with
+    | Unequal_lengths ->
+      error ~msg:(sprintf "%s redeclared param length mismatch" func_name_s) None
+    | Ok res -> res
+  in
+  List.iter param ~f:(fun t ->
+      let d1, d2 = t in
+      if type_cmp d1 d2
+      then ()
+      else error ~msg:(sprintf "%s redeclared param type mismatch" func_name_s) None)
+;;
+
+(* tc_exp will check if an expression is valid
  * including whether the operand and operator consistent. 
  * It will return the type of expression if pass the check.
  * Otherwise, it will report an error and stop. *)
@@ -96,63 +126,69 @@ let rec tc_exp (exp : A.mexp) (env : env) : dtype =
     | A.Less | A.Less_eq | A.Greater | A.Greater_eq ->
       (match tc_exp binop.lhs env, tc_exp binop.rhs env with
       | Int, Int -> Bool
-      | _, _ -> error ~msg:(sprintf "Relation operand should be integer.") loc)
+      | _, _ -> error ~msg:"Relation operand should be integer." loc)
     (* Polyeq op: ==, != *)
     | A.Equal_eq | A.Not_eq ->
       let t1 = tc_exp binop.lhs env in
       let t2 = tc_exp binop.rhs env in
-      if type_cmp t1 t2
-      then Bool
-      else error ~msg:(sprintf "Polyeq operands type mismatch") loc
+      if type_cmp t1 t2 then Bool else error ~msg:"Polyeq operands type mismatch" loc
     (* Rest are int operation *)
     | _ ->
       let t1 = tc_exp binop.lhs env in
       let t2 = tc_exp binop.rhs env in
       (match t1, t2 with
       | Int, Int -> Int
-      | _, _ -> error ~msg:(sprintf "Integer binop operands are not integer") loc))
+      | _, _ -> error ~msg:"Integer binop operands are not integer" loc))
   | A.Terop terop ->
     let t_cond = tc_exp terop.cond env in
     let t1 = tc_exp terop.true_exp env in
     let t2 = tc_exp terop.false_exp env in
     (match t_cond with
-    | Int -> error ~msg:(sprintf "Terop condition should be bool") loc
+    | Int | Void -> error ~msg:"Terop condition should be bool" loc
     | Bool ->
-      if type_cmp t1 t2
-      then t1
-      else error ~msg:(sprintf "Terop true and false exp type should be consistent") loc)
-;;
-
-let trans_ast_dtype = function
-  | A.Int -> Int
-  | A.Bool -> Bool
+      if type_cmp t1 t2 then t1 else error ~msg:"Terop true & false exp type mismatch" loc)
+  | A.Fcall fcall ->
+    let func =
+      match S.find env.funcs fcall.func_name with
+      | None -> error ~msg:"calling a function that not defined" loc
+      | Some s -> s
+    in
+    let expected = List.map func.pars ~f:(fun par -> par.t) in
+    let input = List.map fcall.args ~f:(fun arg -> tc_exp arg env) in
+    tc_signature input expected fcall.func_name;
+    func.ret_type
 ;;
 
 (* 
- * Check AST statements
+ * Check AST statement
  * Type checker will traverse the AST from root recursively.
  * It will report once found an error and then terminate.
  * We only use env to check redefination error.
  * We do not check variable use before initialization in this function.
  * It is done in control flow check.
  * ast: AST transformed from source code
- * env: store the variables state: Init, Decl.
+ * env: store the variables state: Defn, Decl.
+ * func_name: function this current statement belongs to.
  *)
-let rec tc_stms (ast : A.program) (env : env) : env =
+let rec tc_stm (ast : A.mstm) (env : env) (func_name : Symbol.t) : env =
   let loc = Mark.src_span ast in
   match Mark.data ast with
   | A.Assign asn_ast -> tc_assign asn_ast.name loc asn_ast.value env
-  | A.If if_ast -> tc_if if_ast.cond if_ast.true_stm if_ast.false_stm loc env
-  | A.While while_ast -> tc_while while_ast.cond while_ast.body loc env
-  | A.Return ret_ast -> tc_return ret_ast env
+  | A.If if_ast -> tc_if if_ast.cond if_ast.true_stm if_ast.false_stm loc env func_name
+  | A.While while_ast -> tc_while while_ast.cond while_ast.body loc env func_name
+  | A.Return ret_ast -> tc_return ret_ast env func_name
   | A.Nop -> env
   | A.Seq seq_ast ->
-    let env = tc_stms seq_ast.head env in
-    tc_stms seq_ast.tail env
-  | A.Declare decl_ast -> tc_declare decl_ast.t decl_ast.name decl_ast.tail loc env
+    let env = tc_stm seq_ast.head env func_name in
+    tc_stm seq_ast.tail env func_name
+  | A.Declare decl_ast ->
+    tc_declare decl_ast.t decl_ast.name decl_ast.tail loc env func_name
   | A.Sexp sexp ->
-    let _ = (tc_exp sexp env : dtype) in
+    ignore (tc_exp sexp env : dtype);
     env
+  | A.Assert aexp ->
+    let dtype = (tc_exp aexp env : dtype) in
+    if type_cmp dtype Bool then env else error ~msg:"assert exp type expect bool" loc
 
 (* Check following
  * 1) Whether variable name exist in env
@@ -168,55 +204,134 @@ and tc_assign name loc exp env : env =
     if type_cmp exp_type var.dtype
     then (
       (* Type match, Update variable state to initialized. *)
-      let env_vars = S.set env.vars ~key:name ~data:{ var with state = Init } in
+      let env_vars = S.set env.vars ~key:name ~data:{ var with state = Defn } in
       { env with vars = env_vars })
     else
       (* expression and variable type mismatch, error *)
       error ~msg:(sprintf "var type and exp type mismatch") loc
 
-and tc_return mexp env =
-  let loc = Mark.src_span mexp in
-  let exp_type = tc_exp mexp env in
-  match exp_type with
-  | Int -> { env with returns = true }
-  | Bool -> error ~msg:(sprintf "Return type is bool") loc
+and tc_return mexp env func_name =
+  let func = S.find_exn env.funcs func_name in
+  let oracle_type = func.ret_type in
+  let func_name_s = Symbol.name func_name in
+  match mexp with
+  | Some mexp ->
+    let loc = Mark.src_span mexp in
+    let exp_type = tc_exp mexp env in
+    if type_cmp oracle_type exp_type
+    then env
+    else error ~msg:(sprintf "%s return type mismatch" func_name_s) loc
+  | None ->
+    if type_cmp oracle_type A.Void
+    then env
+    else error ~msg:(sprintf "%s ret type expected void, mismatch" func_name_s) None
 
-and tc_if cond true_stm false_stm loc env =
+and tc_if cond true_stm false_stm loc env func_name =
   let cond_type = tc_exp cond env in
   match cond_type with
-  | Int -> error ~msg:(sprintf "If cond type is int") loc
+  | Int | Void -> error ~msg:(sprintf "If cond type is int/void") loc
   | Bool ->
-    let env = tc_stms true_stm env in
-    tc_stms false_stm env
+    let env = tc_stm true_stm env func_name in
+    tc_stm false_stm env func_name
 
-and tc_while cond body loc env =
+and tc_while cond body loc env func_name =
   let cond_type = tc_exp cond env in
   match cond_type with
-  | Int -> error ~msg:(sprintf "While cond type is int") loc
-  | Bool -> tc_stms body env
+  | Int | Void -> error ~msg:(sprintf "While cond type is int/void") loc
+  | Bool -> tc_stm body env func_name
 
-(* Declare check is tricky because we will
- * not override old env. Instead, we will return it
- * once we check the tail. *)
-and tc_declare decl_type decl_name tail loc env =
+(* Declare check is tricky because we will not override old env. 
+ * Instead, we will return it once we check the tail.
+ * We cannot declare a variable with type void. Void can only be used 
+ * as return type for a function. *)
+and tc_declare decl_type decl_name tail loc env func_name =
+  if type_cmp decl_type Void
+  then (
+    let var_name = Symbol.name decl_name in
+    error ~msg:(sprintf "cannot declare variable %s with type void" var_name) None)
+  else ();
   match S.find env.vars decl_name with
   | Some _ -> error ~msg:(sprintf "Declare a variable multiple time") loc
   | None ->
     let vars' =
-      S.add_exn
-        env.vars
-        ~key:decl_name
-        ~data:{ state = Decl; dtype = trans_ast_dtype decl_type }
+      S.add_exn env.vars ~key:decl_name ~data:{ state = Decl; dtype = decl_type }
     in
     let env' = { env with vars = vars' } in
-    let env'' = (tc_stms tail env' : env) in
-    { vars = S.remove env''.vars decl_name; returns = env''.returns }
+    let env'' = (tc_stm tail env' func_name : env) in
+    { env with vars = S.remove env''.vars decl_name }
+;;
+
+(* Rules to follow
+ * 1) parameter variable name should not collide. 
+ * 2) Function may be declared multiple times, in which case 
+ * the declarations must be compatible. Types should be the
+ * same, but parameter name are not required to be the same.
+ *)
+let tc_fdecl ret_type func_name (pars : param list) env =
+  ignore
+    (List.fold_left pars ~init:Set.empty ~f:(fun acc par ->
+         if Set.mem acc par.i
+         then error ~msg:"fdecl parameter name collide" None
+         else Set.add acc par.i)
+      : Set.t);
+  if S.mem env.funcs func_name
+  then (
+    let old_func = S.find_exn env.funcs func_name in
+    let old_dtype = List.map old_func.pars ~f:(fun par -> par.t) in
+    let new_dtype = List.map pars ~f:(fun par -> par.t) in
+    tc_signature (old_func.ret_type :: old_dtype) (ret_type :: new_dtype) func_name;
+    env)
+  else
+    { env with
+      funcs = S.add_exn env.funcs ~key:func_name ~data:{ state = Decl; pars; ret_type }
+    }
+;;
+
+(* Rules to follow
+ * 1) Parameters should not conflict with local variables. We have already elaborated
+ * fdefn, so tc_stm is going to check whether parameter and local variable collide.
+ * 2) Each function can only be defined once.
+ *)
+let tc_fdefn ret_type func_name pars blk env =
+  ignore (tc_stm blk env func_name : env);
+  match S.find env.funcs func_name with
+  | None ->
+    { env with
+      funcs = S.add_exn env.funcs ~key:func_name ~data:{ state = Defn; pars; ret_type }
+    }
+  | Some s ->
+    (match s.state with
+    | Decl ->
+      let old_func = S.find_exn env.funcs func_name in
+      let old_dtype = List.map old_func.pars ~f:(fun par -> par.t) in
+      let new_dtype = List.map pars ~f:(fun par -> par.t) in
+      tc_signature (old_func.ret_type :: old_dtype) (ret_type :: new_dtype) func_name;
+      { env with
+        funcs = S.set env.funcs ~key:func_name ~data:{ state = Defn; pars; ret_type }
+      }
+    | Defn ->
+      error ~msg:(sprintf "function %s already defined." (Symbol.name func_name)) None)
+;;
+
+let rec _typecheck prog env =
+  match prog with
+  | [] -> ()
+  | h :: t ->
+    (match h with
+    | A.Fdecl fdecl ->
+      let env = tc_fdecl fdecl.ret_type fdecl.func_name fdecl.pars env in
+      _typecheck t env
+    | A.Fdefn fdefn ->
+      let env = tc_fdefn fdefn.ret_type fdefn.func_name fdefn.pars fdefn.blk env in
+      _typecheck t env
+    | A.Typedef _ -> _typecheck t env)
 ;;
 
 let typecheck prog =
-  let env = tc_stms prog { vars = S.empty; returns = false } in
-  if not env.returns
-  then (
-    Util.Error_msg.error tc_errors None ~msg:"main does not return";
-    raise Util.Error_msg.Error)
+  let env =
+    { vars = S.empty (* variable decl/def tracker *)
+    ; funcs = S.empty (* function signature *)
+    }
+  in
+  _typecheck prog env
 ;;
