b81ae611592fb57d8a545a615309a9a7880548ef
Tianbo Hao <tianboh@alumni.cmu.edu>
2023-05-31 02:59:55 +0000

    [Feature] Use frame memory to pass parameters
    Pass: l3-large 3193 / 3326
    
    pass: l3-basic all.

--- lab3/var/memory.ml
+++ lab3/var/memory.ml
@@ -13,7 +13,7 @@ module T = struct
   type t =
     { index : int option (* This is a global unique id for memory.*)
     ; base : X86_reg.t
-    ; offset : int (* base + offset is start address of a variable *)
+    ; offset : int (* base + offset * size is start address of a variable *)
     ; size : int (* size of corresponding variable, byte as unit.*)
     }
   [@@deriving sexp, compare, hash]
@@ -22,12 +22,7 @@ end
 include T
 
 let counter = ref 0
-
-let get_allocated_count () =
-  (* printf "now have memory %d\n%!" !counter; *)
-  !counter
-;;
-
+let get_allocated_count () = !counter
 let reset () = counter := 0
 
 let create index base offset size =
@@ -38,7 +33,7 @@ let create index base offset size =
 let get_mem base offset size = { index = None; base; offset; size }
 
 let mem_to_str t =
-  Printf.sprintf "%d(%s)" (-t.offset * t.size) (X86_reg.reg_to_str ~layout:QWORD t.base)
+  Printf.sprintf "%d(%s)" (t.offset * t.size) (X86_reg.reg_to_str ~layout:QWORD t.base)
 ;;
 
 let mem_idx_exn (mem : t) =
