4739f2a06a0d1f91191c871d77bb92684c51d3c7
Tom <tianboh@alumni.cmu.edu>
2025-12-28 08:10:56 +0800

    refac[SSA]: Provide signature for functor
    This signature helps other SSA-based optimization pass construction.

--- lab5/transform/ssa/sig.ml
+++ lab5/transform/ssa/sig.ml
@@ -23,7 +23,38 @@ module type SSAInstrInterface = sig
   val gen_mov : Temp.t -> Temp.t -> instr
 end
 
-module type SSAInterface = sig
-  type phi = Label.t
-  type ssa_bbmap
+module type S = sig
+  type phi = { dst : Temp.t; srcs : Temp.t Label.Map.t }
+  type instr
+  type ssa_instr = Phi of phi | Instr of instr
+  type cfg_bbmap
+  type cfg_set
+
+  type ssa_bb = {
+    label : Label.t;
+    instrs : ssa_instr list;
+    preds : Label.t list;
+    succs : Label.t list;
+  }
+
+  type ssa_bbmap = ssa_bb Label.Map.t
+
+  val collect_tmap : cfg_bbmap -> cfg_set Temp.Map.t
+
+  val insert_phi :
+    Temp.t -> cfg_set -> ssa_bbmap -> cfg_set Label.Map.t -> ssa_bbmap
+
+  val rename : ssa_bbmap -> Label.t -> cfg_set Label.Map.t -> ssa_bbmap
+  val to_ssa : cfg_bbmap -> ssa_bbmap
+  val from_ssa : ssa_bbmap -> cfg_bbmap
 end
+
+module type Make = functor
+  (Instr : SSAInstrInterface)
+  (Cfg : Analysis_cfg.Sig.CFGInterface with type i = Instr.instr)
+  (Dom : Analysis_dominator.Sig.DominatorInterface with type bbmap = Cfg.bbmap)
+  ->
+  S
+    with type instr = Instr.instr
+     and type cfg_bbmap = Cfg.bbmap
+     and type cfg_set = Cfg.set
